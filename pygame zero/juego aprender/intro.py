e5c9a08b2b2c9cd50b3180cc213706f6fa8c067f
janopineda <janopineda@gmail.com>
2024-05-07 15:15:56 -0600

    Modificaciones

--- pygame zero/juego aprender/intro.py
+++ pygame zero/juego aprender/intro.py
@@ -1,8 +1,9 @@
 """pip install git+https://github.com/QuirkyCort/pgzhelper"""
 import pgzrun
 from pgzhelper import *
+import sys
 from random import randint
-import time
+
 
 
 # tamaño de pagina
@@ -11,38 +12,54 @@ HEIGHT = 600
 
 chico =Actor( 'chavo1')
 chico.images = ['chavo1','chavo2','chavo3']
-chico.fps = 5
+chico.fps = 7
+
+music.play('rola2')
+
+fondo=Actor('fondo')
 
 def draw():
     screen.clear()
+    fondo.draw()
     chico.draw()
     
 
 def update():
-    chico.animate()
-    """if keyboard.w:
-        chico.y -= 5
+    movimiento_chico()
+    limite_chico()
+
+def limite_chico():
+    
+    if chico.x >800:
+        chico.x = 800
+    if chico.x <1:
+        chico.x = 1
+    if chico.y > 600:
+        chico.y = 600   
+    if chico.y <1:
+        chico.y = 1
+
+def movimiento_chico():
+    #chico.image="chavo1"
+    if keyboard.w:
+        chico.y -= 2
+        chico.animate()
+
     elif keyboard.s:
-        chico.y += 5
+        chico.y += 2
+        chico.animate()
+
     elif keyboard.a:
-        chico.x -= 5
+        chico.x -= 2
+        chico.animate()
         chico.flip_x = True
 
     elif keyboard.d:
-        chico.x += 5
+        chico.x += 2
+        chico.animate()
         chico.flip_x = False
 
 
-    if chico.x >523:
-        chico.x = 523
-    if chico.x <1:
-        chico.x = 1
-    if chico.y > 307:
-        chico.y = 307
-    if chico.y <1:
-        chico.y = 1"""
 
-def new_func():
-    chico.animate()
     
 pgzrun.go()
