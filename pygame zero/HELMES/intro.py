23b112296657e87230da634996c0b0c478bac7ae
janopineda <janopineda@gmail.com>
2024-05-07 22:59:53 -0600

    subiendo material para vane

---
+++ pygame zero/HELMES/intro.py
@@ -0,0 +1,48 @@
+"""
+Libreria para pgzhelper
+pip install git+https://github.com/QuirkyCort/pgzhelper
+pip install pgzero
+
+"""
+import pgzrun
+from pgzhelper import *
+import os
+
+
+fondo=Actor('fondo')
+fondo.images = ['fondo','fondo2']
+
+boton1=Actor('boton')
+boton1.pos = 800/3, 600/2
+#boton1.opacity=0.1
+
+
+boton2=Actor('boton')
+boton2.pos = 800/1.5, 600/2
+
+boton3=Actor('miau')
+boton3.pos = -200, 0
+
+def draw():
+    screen.clear()
+    fondo.draw()
+    boton1.draw()
+    boton2.draw()
+    boton3.draw()
+
+def update():
+    #fondo.animate()
+    pass
+
+def on_mouse_down(pos):
+    if boton1.collidepoint(pos):
+        fondo.image='fondo2'
+        boton1.pos = -200, 0
+        boton2.pos = -200, 0
+        sounds.cat.play()
+        boton3.pos = 800/2, 600/2
+    else:
+        print("hey man nooo")
+
+os.environ['SDL_VIDEO_CENTERED'] = '1' # centrar pantalla
+pgzrun.go()
