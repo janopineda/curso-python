14d9286e482f6a1739c77d9696e17fea968b6afd
janopineda <janopineda@gmail.com>
2024-05-17 09:44:04 -0600

    segmentación del programa

---
+++ tkinter/proyectosistema/login.py
@@ -0,0 +1,42 @@
+import var as conf
+from tkinter import *
+import os
+
+def win():
+    root=Tk()
+    root.configure(bg="#58B22E")
+    root.geometry(conf.tamanoVentana)
+
+    ventana=Frame(root)
+    ventana.configure(background="#58B22E")
+    ventana.pack()
+            
+    logoIMG=PhotoImage(file=os.path.join(conf.ubImg,"logo.png"))
+    logo=logoIMG.subsample(3)
+
+    logoC=Label(ventana,image=logo, bg="#58B22E")
+    logoC.grid(row=1, column=1)
+
+    userlba=Frame(ventana, background="#FF00E2")
+    userlba.grid(row=10, column=1)
+
+    User=Label(userlba, text="Usuario",bg="#FF00E2", padx=15, pady=10)
+    User.configure(font="Arial 12 bold" , bg="#FF00E2")
+    User.grid(row=11,column=1)
+
+    UserEntry=Entry(userlba)
+    UserEntry.configure(width=25)
+    UserEntry.grid(row=12,column=1,padx=15, pady=2)
+
+    Pw=Label(userlba, text="Contraseña",bg="#FF00E2", padx=15, pady=2)
+    Pw.configure(font="Arial 12 bold" , bg="#FF00E2")
+    Pw.grid(row=13,column=1)
+
+    PwEntry=Entry(userlba)
+    PwEntry.configure(width=25)
+    PwEntry.grid(row=14,column=1,padx=15, pady=2)
+
+    boton=Button(userlba,text="Entrar", width=15, height=2, background="#B2009E")
+    boton.config(bd=0)
+    boton.grid(row=16,column=1,padx=15, pady=20)
+    root.mainloop()
--- tkinter/proyectosistema/main.py
+++ tkinter/proyectosistema/main.py
@@ -1,48 +1,12 @@
-import var as conf
 from tkinter import *
-import os
+import login as Wlogin
+
 class sistema:
 
     def __init__(self):
-        root=Tk()
-        root.configure(bg="#58B22E")
-        root.geometry(conf.tamanoVentana)
-
-        ventana=Frame(root)
-        ventana.configure(background="#58B22E")
-        ventana.pack()
         
-        logoIMG=PhotoImage(file=os.path.join(conf.ubImg,"logo.png"))
-        logo=logoIMG.subsample(3)
-
-        logoC=Label(ventana,image=logo, bg="#58B22E")
-        logoC.grid(row=1, column=1)
-
-        userlba=Frame(ventana, background="#FF00E2")
-        userlba.grid(row=10, column=1)
-
-        User=Label(userlba, text="Usuario",bg="#FF00E2", padx=15, pady=10)
-        User.configure(font="Arial 12 bold" , bg="#FF00E2")
-        User.grid(row=11,column=1)
-
-        UserEntry=Entry(userlba)
-        UserEntry.configure(width=25)
-        UserEntry.grid(row=12,column=1,padx=15, pady=2)
-
-        Pw=Label(userlba, text="Contraseña",bg="#FF00E2", padx=15, pady=2)
-        Pw.configure(font="Arial 12 bold" , bg="#FF00E2")
-        Pw.grid(row=13,column=1)
-
-        PwEntry=Entry(userlba)
-        PwEntry.configure(width=25)
-        PwEntry.grid(row=14,column=1,padx=15, pady=2)
-
-        boton=Button(userlba,text="Entrar", width=15, height=2, background="#B2009E")
-        boton.config(bd=0)
-        boton.grid(row=16,column=1,padx=15, pady=20)
-        root.mainloop()
-
-
+        Wlogin.win()
+        
 run=sistema()
 
 
