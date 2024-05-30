6fc7d1660b30bd517e5a1633fa1107463d87c73b
janopineda <janopineda@gmail.com>
2024-05-30 10:55:55 -0600

    se actualizo juego pong

--- pygame zero/fut ping pong/index.py
+++ pygame zero/fut ping pong/index.py
@@ -10,6 +10,9 @@ HEIGHT = 307
 
 pelota=True
 velocidad=2
+score1 = 0
+score2 = 0
+sonidoInicio=True
 randon=randint(1,2)
 
 bg =Actor( 'fondo')
@@ -17,50 +20,82 @@ bg =Actor( 'fondo')
 jugadorRojo =Actor( 'jugadoruno')
 jugadorRojo.pos = 20, 307/2
 
+jugadorVerde =Actor( 'jugadordos')
+jugadorVerde.pos = 508, 307/2
+
 balon=Actor('balon')
 balon.pos= 524/2 , 307/2
 
+MarcaGolizq=Actor('marcagol')
+MarcaGolizq.pos= 1 , 156
 
+MarcaGolDer=Actor('marcagol')
+MarcaGolDer.pos= 521 , 156
+
+music.play('rola2')
+music.set_volume(.3)
 
 def draw():
     screen.clear()
     bg.draw()
+    screen.draw.text('Puntos: ' + str(score1) + ' / ' + str(score2), (15,10), color=(255,255,255), fontsize=30)
+    MarcaGolDer.draw()
+    MarcaGolizq.draw()
     jugadorRojo.draw()
+    jugadorVerde.draw()
     balon.draw()
+
+    
 
 def update():
     global pelota
     global randon
     global velocidad
+    global score2
+    global score1
+    global sonidoInicio
 
+
+    if sonidoInicio==True:
+        sonido()
 
     if pelota == True:
         balon.x += velocidad
         balon.y -= randon
+
     if pelota == False:
         balon.x -= velocidad
         balon.y -= randon
         
-    if balon.x > 523:
+    if balon.x > 523 :
         pelota = False
         balon.x = 520
+        sounds.rebote.play()
     if balon.x < 0:
         pelota = True
         balon.x=10
+        sounds.rebote.play()
 
     if balon.y < 0:
         balon.y =10
         randon = -abs(randon)
+        sounds.rebote.play()
 
-    if balon.y > 307:
+    if balon.y >307 :
         balon.y =307
         randon = +abs(randon)
+        sounds.rebote.play()
 
 
     if keyboard.w:
         jugadorRojo.y -= 5
     elif keyboard.s:
         jugadorRojo.y += 5
+
+    if keyboard.UP:
+        jugadorVerde.y -= 5
+    elif keyboard.DOWN:
+        jugadorVerde.y += 5
 
     if jugadorRojo.x >523:
         jugadorRojo.x = 523
@@ -70,9 +105,50 @@ def update():
         jugadorRojo.y = 307
     if jugadorRojo.y <1:
         jugadorRojo.y = 1
+    
+    if jugadorVerde.x >523:
+        jugadorVerde.x = 523
+    if jugadorVerde.x <1:
+        jugadorVerde.x = 1
+    if jugadorVerde.y > 307:
+        jugadorVerde.y = 307
+    if jugadorVerde.y <1:
+        jugadorVerde.y = 1
 
     if jugadorRojo.colliderect(balon):
         pelota=True
-        velocidad=velocidad+.8
+        sounds.rebote.play()
+        if velocidad < 14 :
+            velocidad=velocidad+.4
+            print(velocidad)
+    if jugadorVerde.colliderect(balon):
+        pelota=False
+        sounds.rebote.play()
+        if velocidad < 14 :
+            velocidad=velocidad+.4
+            print(velocidad)
+    if MarcaGolizq.colliderect(balon):
+        balon.pos= 524/2 , 307/2
+        velocidad=1
+        score1 += 1
+        pelota=True
+        sounds.muerte.play()
+
+    if MarcaGolDer.colliderect(balon):
+        balon.pos= 524/2 , 307/2
+        velocidad=1
+        score2 += 1
+        pelota=False
+        sounds.muerte.play()
+
+def sonido():
+    global sonidoInicio
+    sounds.inicio.play()
+    sonidoInicio=False
+
+"""def on_mouse_down(pos):
+    global Translado
+    if boton1.collidepoint(pos):
+        fondo.image='fondo2'"""
 
 pgzrun.go()
