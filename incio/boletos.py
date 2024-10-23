5fcf75c33e7803e5348dae4c7da3410ecb8ec923
janopineda <janopineda@gmail.com>
2024-10-23 08:54:34 -0600

    programa de asientos

---
+++ incio/boletos.py
@@ -0,0 +1,7 @@
+x = list(range(1, 10)), list(range(11,20))
+print(x)
+print(x[1][8])
+
+
+x[0][1] = "X"
+print(x)
--- incio/programa cine.py
+++ incio/programa cine.py
@@ -3,6 +3,8 @@ total=0
 COSTO=90
 opciones=None
 sele=None
+selecasiento=None
+y=list(range(1,9)) , list(range(1,9)) 
 
 while opciones != "s":
     print("a) Comprar")
@@ -15,8 +17,21 @@ while opciones != "s":
     # opcion de compra de boleto
     if opciones == "a" or opciones == "comprar":
         print(f"el costo del boleto es de {COSTO}")
-        boletos=int(input("Cuantos boletos quieres : \n"))
+        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+        boletos=int(input("Cuantos boletos quieres : "))
+        selecfila=input("Dame la fila")
+        for asiento in range(0,9):
+            print(asiento)
+        if selecfila.lower()=="a":
+            asiento=int(input("Dame el asiento"))
+            asientoselec=asiento-1
+            if asientoselec<7:
+                asientoselec
+        if selecfila.lower()=="b":
+            asiento=int(input("Dame el asiento"))
         total=boletos*COSTO
+        print(f"El asiento es {selecasiento}")
         print(f"El total es: {total}")
 
     if opciones == "b" or opciones == "promociones":
@@ -24,7 +39,7 @@ while opciones != "s":
         print("b) Miercoles de promo ")
         sele=input("Seleciona la opcion ")
         if sele=="a":
-            print("Has selecionado tarjeta")
+            print("Has seleccionado tarjeta")
             promo1=total*0.5
             total=promo1
             print(f"Vas a pagar ${total}")
@@ -34,4 +49,4 @@ while opciones != "s":
             total=promo2
     if opciones == "c" or opciones=="cancelar":
         print("se a cancelado la compra ")
-        total=0*0
+        total=0
