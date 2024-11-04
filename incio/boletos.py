cfab3d8d9d9356b2096ae108f0fd1f1d7e772efe
janopineda <janopineda@gmail.com>
2024-11-04 15:03:12 -0600

    acualiza

--- incio/boletos.py
+++ incio/boletos.py
@@ -1,7 +1,21 @@
-x = list(range(1, 10)), list(range(11,20))
-print(x)
-print(x[1][8])
+y=list(range(1,9)) , list(range(1,9)) 
 
+while True: #XD
+    print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+    print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
 
-x[0][1] = "X"
-print(x)
+    x=int(input("Dame la silla : ")) 
+    alma=x-1
+
+    while y[0][alma] == "X":
+        print("")
+        print("La silla esta ocupada por un intruso")
+        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
+        x=int(input("Dame la silla : ")) 
+        alma=x-1
+    else:
+        y[0][alma] = "X"
+        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
+
--- incio/programa cine.py
+++ incio/programa cine.py
@@ -4,6 +4,10 @@ COSTO=90
 opciones=None
 sele=None
 selecasiento=None
+Nombre=[]
+Telefono=[]
+Compra=[]
+Bolet=[]
 y=list(range(1,9)) , list(range(1,9)) 
 
 while opciones != "s":
@@ -21,6 +25,11 @@ while opciones != "s":
         print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
         print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
         boletos=int(input("Cuantos boletos quieres : "))
+        DatoNombre=input("Dame el nombre : ")
+        DatoTelefono=input("Dame el Telefon : ")
+        Nombre.append(DatoNombre)
+        Bolet.append(boletos)
+        Telefono.append(DatoTelefono)
         selecfila=input("Dame la fila : ")
 
         if selecfila.lower()=="a":
@@ -28,9 +37,13 @@ while opciones != "s":
                 silla=int(input(f"Dame la silla {f+1} :"))
                 try:
                     sentarse=silla-1
-                    y[0][sentarse] = "X"
-                    print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-                    print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+                    while y[0][sentarse]=="X":
+                        silla=int(input(f"Dame la silla {f+1} :"))
+                        sentarse=silla-1
+                    else:
+                        y[0][sentarse] = "X"
+                        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+                        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
                 except:
                     print("Esta silla no existe en la fila")
                     print("Reinicia toda la compra")
