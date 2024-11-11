3ddd83e6a46c0be482a2bcd27aebf8b7778df34e
janopineda <janopineda@gmail.com>
2024-11-11 10:29:30 -0600

    proyecto cine

--- incio/boletos.py
+++ incio/boletos.py
@@ -1,31 +1,45 @@
 y=list(range(1,9)) , list(range(1,9)) 
 contar=0
-
+contador1=0
+contador2=0
 while True: 
-    print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-    print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
-    
-    contar=0 # reinicia el valor de el conteo de asientos
-    for x in range(len(y[0])):
-        if y[0][x]=="X":
-            contar=contar+1 # cuenta las sillas que estan ocupadas
-    print(f"asientos ocupados en la fila A {contar}")
-    
-    if contar==8:
-        print("Esta llena la fila")
-        input("Continuar")
-    else:
-        x=int(input("Dame la silla : ")) 
-        alma=x-1
-        while y[0][alma] == "X":
-            print("")
-            print("La silla esta ocupada por un intruso")
-            print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-            print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
-            x=int(input("Dame la silla : ")) 
-            alma=x-1
+    print ("a) compra de boletos")
+    seleccion=input("Dame la opcion ")
+
+    if seleccion.lower() == "a":
+        contador1=0
+        contador2=0
+        for i in range( len(y[0]) ):
+            if y[0][i] == "X":
+                contador1=contador1+1
+            if y[1][i] == "X":
+                contador2=contador2+1
+        if contador1==8 and contador2==8:
+            print("la sala se lleno")
         else:
-            y[0][alma] = "X"
             print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-            print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
-    
+            print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+            fila=input("Dame la fila A/B")
+            if fila.lower()=="a":
+                contadorfilaa=0
+                for i in range(len(y[0])):
+                    if y[0][i]=="X":
+                        contadorfilaa=contadorfilaa+1
+                if contadorfilaa==8:
+                    print("Fila a esta llena")
+                    input("Presiona enter para continuar")
+                else:
+                    x=int(input("Dame la silla : "))
+                    alma=x-1
+                    while y[0][alma] == "X":
+                        print("")
+                        print("La silla esta ocupada por un intruso")
+                        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+                        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
+                        x=int(input("Dame la silla : ")) 
+                        alma=x-1
+                    else:
+                        y[0][alma] = "X"
+                        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+                        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}")
+                    
--- incio/programa cine.py
+++ incio/programa cine.py
@@ -9,86 +9,122 @@ Telefono=[]
 Compra=[]
 Bolet=[]
 contador=0
+contador1=0
+contador2=0
 y=list(range(1,9)) , list(range(1,9)) 
 
 while opciones != "s":
     print("a) Comprar")
-    print("b) Promociones")
-    print("c) cancelar boleto")
-    print("d) Reporte de compras de boletos")
+    print("b) cancelar boleto")
+    print("c) Reporte de compras de boletos")
     print("s) Salir S/s\n")
     seleccion=input("Selecione la opcion : ")
     opciones=seleccion.lower()
 
     # opcion de compra de boleto
     if opciones == "a" or opciones == "comprar":
-        print(f"el costo del boleto es de {COSTO}")
-        print("Filas disponibles")
-        print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-        print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
-        boletos=int(input("Cuantos boletos quieres : "))
-        DatoNombre=input("Dame el nombre : ")
-        DatoTelefono=input("Dame el Telefon : ")
-        Nombre.append(DatoNombre)
-        Bolet.append(boletos)
-        Telefono.append(DatoTelefono)
-        selecfila=input("Dame la fila : ")
+        contador1=0
+        contador2=0
+        for x in range(len(y[0])):
+            if y[0][x]=="X":
+                contador+1
+        for x in range(len(y[1])):
+            if y[1][x]=="X":
+                contador2+1
+        if contador==8 and contador2==8:
+            print("la sala se lleno ")
+            input("Presiona para continuar")
+        else:
+            print(f"el costo del boleto es de {COSTO}")
+            print("Filas disponibles")
+            print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+            print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+            boletos=int(input("Cuantos boletos quieres : "))
+            DatoNombre=input("Dame el nombre : ")
+            DatoTelefono=input("Dame el Telefon : ")
+            Nombre.append(DatoNombre)
+            Telefono.append(DatoTelefono)
+            selecfila=input("Dame la fila : ")
 
-        if selecfila.lower()=="a":
-            contador=0
-            for x in range( len(y[0]) ):
-                if y[0][x]=="X":
-                    contador=contador+1
+            if selecfila.lower()=="a":
+                contador=0
+                for x in range( len(y[0]) ):
+                    if y[0][x]=="X":
+                        contador=contador+1
 
-            if contador==8:
-                print("la fila A esta llena ")
-            else:
-                for f in range(boletos):
-                    silla=int(input(f"Dame la silla {f+1} :"))
-                    try:
-                        sentarse=silla-1
-                        while y[0][sentarse]=="X":
-                            print("Esta ocupado seleccione otro")
-                            silla=int(input(f"Dame la silla {f+1} :"))
+                if contador==8:
+                    print("la fila A esta llena ")
+                    input("Da enter para continuar")
+                    boletos=0
+                    Bolet.append(boletos)
+                else:
+                    for f in range(boletos):
+                        silla=int(input(f"Dame la silla {f+1} :"))
+                        try:
                             sentarse=silla-1
-                        else:
-                            y[0][sentarse] = "X"
-                            print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-                            print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
-                    except:
-                        print("Esta silla no existe en la fila")
-                        print("Reinicia toda la compra")
-                        boletos=0 
+                            while y[0][sentarse]=="X":
+                                print("Esta ocupado seleccione otro")
+                                silla=int(input(f"Dame la silla {f+1} :"))
+                                sentarse=silla-1
+                            else:
+                                y[0][sentarse] = "X"
+                                print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+                                print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+                        except:
+                            print("Esta silla no existe en la fila")
+                            print("Reinicia toda la compra")
+                            boletos=0 
+                    Bolet.append(boletos)
+                    print("boletos ",boletos)
 
-        if selecfila.lower()=="b":
-            for f in range(boletos):
-                silla=int(input(f"Dame la silla {f+1} :"))
-                try:
-                    sentarse=silla-1
-                    y[1][sentarse] = "X"
-                    print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
-                    print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
-                except:
-                    print("Esta silla no existe en la fila")
-                    print("Reinicia toda la compra")
-                    boletos=0 
+            if selecfila.lower()=="b":
+                contador=0
+                for x in range( len(y[0]) ):
+                    if y[1][x]=="X":
+                        contador=contador+1
 
-        total=boletos*COSTO
-        print(f"El total es: {total}")
+                if contador==8:
+                    print("la fila A esta llena ")
+                    input("Da enter para continuar")
+                    boletos=0
+                    Bolet.append(boletos)
+                else:
+                    for f in range(boletos):
+                        silla=int(input(f"Dame la silla {f+1} :"))
+                        try:
+                            sentarse=silla-1
+                            while y[1][sentarse]=="X":
+                                print("Esta ocupado seleccione otro")
+                                silla=int(input(f"Dame la silla {f+1} :"))
+                                sentarse=silla-1
+                            else:
+                                y[1][sentarse] = "X"
+                                print(f"Fila A:   {y[0][0]}   {y[0][1]}   {y[0][2]}   {y[0][3]}   {y[0][4]}   {y[0][5]}   {y[0][6]}   {y[0][7]}") 
+                                print(f"Fila B:   {y[1][0]}   {y[1][1]}   {y[1][2]}   {y[1][3]}   {y[1][4]}   {y[1][5]}   {y[1][6]}   {y[1][7]}") 
+                        except:
+                            print("Esta silla no existe en la fila")
+                            print("Reinicia toda la compra")
+                            boletos=0 
+            submenu=input("Deseas Promociones Si/No :")
+            if submenu.lower() == "si":
+                print("Estas son las promociones")
+                print("a) Tienes tarjeta ")
+                print("b) Miercoles de promo ")
+                sele=input("Seleciona la opcion ")
+                if sele=="a":
+                    print("Has seleccionado tarjeta")
+                    promo1=total*0.5
+                    total=promo1
+                    print(f"Vas a pagar ${total}")
+                if sele=="b":
+                    print("Miercoles de %30")
+                    promo2=total*0.3
+                    total=promo2
+            if submenu.lower() == "no":
+                Bolet.append(boletos)
+                total=boletos*COSTO
+                print(f"El total es: {total}")
 
-    if opciones == "b" or opciones == "promociones":
-        print("a) Tienes tarjeta ")
-        print("b) Miercoles de promo ")
-        sele=input("Seleciona la opcion ")
-        if sele=="a":
-            print("Has seleccionado tarjeta")
-            promo1=total*0.5
-            total=promo1
-            print(f"Vas a pagar ${total}")
-        if sele=="b":
-            print("Miercoles de %30")
-            promo2=total*0.3
-            total=promo2
-    if opciones == "c" or opciones=="cancelar":
+    if opciones == "b" or opciones=="cancelar":
         print("se a cancelado la compra ")
         total=0
