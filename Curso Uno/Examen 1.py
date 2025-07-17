3f7bad00d0998b28a4352a17bf36de5128247e58
janopineda <janopineda@gmail.com>
2025-07-17 10:42:30 -0600

    subir examen

---
+++ Curso Uno/Examen 1.py
@@ -0,0 +1,243 @@
+opciones=True
+punto=int(0)
+while opciones :
+    print("a ) Bullyng" )
+    print("b ) Gestión del estrés" )
+    print("c ) Reportar" )
+    print("d ) Bullyng" )
+    opciones=str(input("Dame la opcion :")[0])
+
+    if opciones.lower()=="a":
+        print("1. Me esconden las cosas:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("2. Se ríen de mí cuando me equivoco:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("3. Le dicen a otros que no hablen o estén conmigo:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("4. Me insultan:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("5. Me chillan o gritan:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("6. Me acusan de cosas que no he dicho o hecho:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("7. Me amenazan:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("8. Me empujan:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("9. Se meten conmigo para hacerme llorar:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("10. Se burlan de mi apariencia física:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("11. Hacen gestos de burla o desprecio hacia mí:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("11. Hacen gestos de burla o desprecio hacia mí:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+        
+        print("11. Me dan collejas o tortazos:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("12. Me insultan para burlarse de mí:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("13. Se meten conmigo por mi forma de ser:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("14. Van por ahí contando mentiras sobre mí:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("15. Se meten conmigo por ser diferente:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("16. Intentan que me castiguen:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("16. Me desprecian:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        print("17. Me dan puñetazos o patadas:")
+        print("a) Nunca  b) Pocas veces  c) Muchas veces")
+        opt=str(input()[0])
+
+        if opt.lower()=="a":
+            punto=punto+1
+        elif opt.lower=="b":
+            punto=punto+2
+        elif opt.lower()=="c":
+            punto=punto+3
+
+        if punto < 18:
+            print("Realemte no tienes bullyng")
+        elif punto > 17 and punto < 35:
+            print("estas puede que es inicios al bullyng")
+        elif punto > 34 and punto < 52:
+            print("es definitivamente necesita atencion ")
+
+    if opciones.lower()=="b":
+        pass
+    if opciones.lower()=="c":
+        pass
+    if opciones.lower()=="d":
+        pass
